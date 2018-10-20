480bbc4ac4e0d137927541b17b06490897ae56db
Yuvraj Jhala <png625@gmail.com>
2018-10-20 17:37:05 +0530

    FEATURE: added deploy script

--- public/deploy.php
+++ public/deploy.php
@@ -5,5 +5,8 @@
  * Date: 20/10/18
  * Time: 5:23 PM
  */
-$path = __DIR__;
-exec("cd $path && sudo git pull origin master");
+$path = __DIR__ . '/..';
+exec("cd $path && sudo git pull origin master", $output,$error);
+
+print('<pre>');
+print_r($output);
