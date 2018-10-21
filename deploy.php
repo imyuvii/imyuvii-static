e6e12f5ac8d37088462362672f1bcfe59d28b895
Yuvraj Jhala <png625@gmail.com>
2018-10-21 08:31:20 +0530

    FEATURE: deploy script modified

--- deploy.php
+++ deploy.php
@@ -5,5 +5,12 @@
  * Date: 20/10/18
  * Time: 5:23 PM
  */
-$path = __DIR__;
-echo exec("cd $path && sudo git pull origin master");
+exec("(/var/www/base.sh) 2>&1", $output, $result);
+
+if($result != 0){
+    //if $result is different to 0 -> something wrong and display errors
+    print_r([$output,$result]);
+}else{
+    //if $result == 0 -> Should be OK and continue with your code here
+    echo 1;
+}
